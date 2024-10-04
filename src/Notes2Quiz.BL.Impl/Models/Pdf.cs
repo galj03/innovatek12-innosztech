3639aa63d345da1a5935709189b51ea29d255f3d
Gál József <galjozsi45@gmail.com>
2024-10-04 16:51:59 +0200

    Add remaining model

---
+++ src/Notes2Quiz.BL.Impl/Models/Pdf.cs
@@ -0,0 +1,20 @@
+﻿using Notes2Quiz.BL.Models;
+
+namespace Notes2Quiz.BL.Impl.Models
+{
+    internal class Pdf : IPdf
+    {
+        #region Properties
+        public string Name { get; }
+        public byte[] Data { get; }
+        #endregion
+
+        #region ctor
+        public Pdf(string name, byte[] data)
+        {
+            Name = name;
+            Data = data;
+        }
+        #endregion
+    }
+}
