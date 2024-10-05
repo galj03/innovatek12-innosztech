1633282242e36a1ae09b8752a5ce304cec1242e2
Gál József <galjozsi45@gmail.com>
2024-10-05 23:52:38 +0200

    Implement quiz generation from pdf
    Add controller docs

--- src/Notes2Quiz.Web.API/Controllers/_Interfaces/IQuizController.cs
+++ src/Notes2Quiz.Web.API/Controllers/_Interfaces/IQuizController.cs
@@ -10,10 +10,22 @@ namespace Notes2Quiz.Web.API.Controllers
     /// </summary>
     public interface IQuizController
     {
+        /// <summary>
+        /// Creates and returns a quiz from the data found in the given pdf file.
+        /// </summary>
+        /// <returns>A quiz object.</returns>
         Task<ActionResult<IQuiz>> ParsePdf([Required][FromForm] FileInputDTO fileInputDTO);
 
+        /// <summary>
+        /// Creates and returns a quiz from the data found in the given json object.
+        /// </summary>
+        /// <returns>A quiz object.</returns>
         Task<ActionResult<IQuiz>> ParseText([Required] TextInputDTO text);
 
+        /// <summary>
+        /// Creates and returns a quiz from the data found on the given images.
+        /// </summary>
+        /// <returns>A quiz object.</returns>
         Task<ActionResult<IQuiz>> ParseImages([Required] IEnumerable<IFormFile> images);
     }
 }
